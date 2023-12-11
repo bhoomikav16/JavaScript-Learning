ed398948717bb0ea3f3abaf04d59f727fe81db2e
LAPTOP-BHOOMIKA\Bhoomika <bhoomikav16>
2023-12-11 17:33:48 +0530

    Intial Commit on JS

---
+++ assets/scripts/script1.js
@@ -0,0 +1,17 @@
+alert ('this works'); //How to add scripts in the website
+// Variable concepts
+
+let currentResult=100;
+// let is variable to declare the value
+// it acts as an container to store the value
+// value can be modified again
+
+const Result=200;
+//const is a variable to store to value
+//const cannot be modified again
+
+//introduction to function
+// function GreetName(name){
+// alert('Hi +name)
+// }
+
