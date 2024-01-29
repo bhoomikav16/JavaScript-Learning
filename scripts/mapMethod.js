5d1a48bb0622ae9a089c5b5833454b0d1e332910
LAPTOP-BHOOMIKA\Bhoomika <bhoomikav16>
2024-01-29 11:47:32 +0530

    Map method

---
+++ scripts/mapMethod.js
@@ -0,0 +1,13 @@
+const fruits=new Map();
+fruits.set("apples",1300);
+fruits.set("banana", 200);
+fruits.set("grapes",300);
+fruits.set("apples",500);
+fruits.delete("apples")
+console.log("fruits name / value", + fruits.get("grapes"));
+
+let text="";
+fruits.forEach(function(value,key){
+    text+=key+'='+value+';';
+})
+console.log("new output:", text);
